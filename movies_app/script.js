58b6b0d5217ef1549f2d9bf3d5858a2d8d610e95
Alex David <alexz.chimi@gmail.com>
2023-12-10 16:39:52 +0200

    search feature added

--- movies_app/script.js
+++ movies_app/script.js
@@ -1,14 +1,29 @@
 const APIURL =
     'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2';
-const main = document.querySelector('main');
+const SEARCHAPI =
+    'https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query=';
 
-async function getMovie() {
-    const res = await fetch(APIURL);
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const search = document.getElementById('search');
+
+//initially get fac movies
+getMovie(APIURL);
+
+async function getMovie(url) {
+    const res = await fetch(url);
     const data = await res.json();
 
-    data.results.forEach((movie) => {
-        const { title, vote_average, poster_path } = movie;
+    console.log(data);
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    //clear main
+    main.innerHTML = '';
+    movies.forEach((movie) => {
+        const { title, vote_average, poster_path, overview } = movie;
         const movieEl = document.createElement('div');
 
         movieEl.classList.add('movie');
@@ -19,13 +34,33 @@ async function getMovie() {
 		/>
 		<div class="movie-info">
 			<h3>${title}</h3>
-			<span>${vote_average}</span>
+			<span class="${getClassByRate(vote_average)}">${vote_average}</span>
+			
 		</div>
+		<div class="overview">
+		<h4>Overview:</h4>
+		${overview}</div>
 		`;
         main.appendChild(movieEl);
     });
-    console.log(data);
-    return data;
 }
 
-getMovie();
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if (searchTerm) {
+        getMovie(SEARCHAPI + searchTerm);
+        search.value = '';
+    }
+});
