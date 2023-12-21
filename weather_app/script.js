3105712d1c7a987e55d8f3023aa54555cdf2bfa2
Alex David <alexz.chimi@gmail.com>
2023-12-21 21:24:45 +0200

    changed api

--- weather_app/script.js
+++ weather_app/script.js
@@ -3,18 +3,29 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 
 const APIKEY = config.apiKey;
-const APIURL = (city) =>
-    `https://api.tomorrow.io/v4/weather/realtime?location=${city}&units=metric&apikey=${APIKEY}`;
 
-async function getWeatherByLocation(city) {
-    const res = await fetch(APIURL(city));
+const COORDAPI = (city) =>
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKEY}`;
+
+const WEATHERAPI = (lat, lon) =>
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`;
+
+async function getCoordinates(city) {
+    const res = await fetch(COORDAPI(city));
+    const data = await res.json();
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+    getWeatherByLocation(lat, lon);
+}
+async function getWeatherByLocation(lat, lon) {
+    const res = await fetch(WEATHERAPI(lat, lon));
     const data = await res.json();
     addWeatherDataToPage(data);
 }
 
 function addWeatherDataToPage(data) {
-    const temp = data.data.values.temperature;
-    const iconT = data.data.values.weatherCode;
+    const temp = Math.floor(data.main.temp);
+    const imgCode = data.weather[0].icon;
 
     const weather = document.createElement('div');
     weather.classList.add('weather');
@@ -22,19 +33,18 @@ function addWeatherDataToPage(data) {
     weather.innerHTML = `
 	<h2>${temp}°C</h2>
 	<small>in ${search.value}</small>
-	<img>${iconT}</img>
+	<img src="https://openweathermap.org/img/wn/${imgCode}@2x.png" >
+	
 	`;
-    main.innerHTML = '';
+    // main.innerHTML = '';
 
     main.appendChild(weather);
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const city = search.value;
-
     if (city) {
-        getWeatherByLocation(city);
+        getCoordinates(city);
     }
 });
